5e36cd385efed7aff0ce65aa06a82a9f21dc7cbc
naliwka <avierina.nataliia@gmail.com>
2023-03-14 15:21:25 +0200

    Lab_3-1 is done

--- Lab_3-1/Program.cs
+++ Lab_3-1/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab_3_1
 {
@@ -10,6 +9,70 @@ namespace Lab_3_1
     {
         static void Main(string[] args)
         {
+            string filePath = "transactions.csv";
+            string dateFormat = "dd/MM/yyyy";
+            int batchSize = 10;
+
+            Func<string, DateTime> getDate = (string transaction) =>
+            {
+                string[] fields = transaction.Split(',');
+                return DateTime.ParseExact(fields[0], dateFormat, null);
+            };
+
+            Func<string, double> getAmount = (string transaction) =>
+            {
+                string[] fields = transaction.Split(',');
+                return Double.Parse(fields[1]);
+            };
+
+            Action<DateTime, double> displayTotal = (DateTime date, double total) =>
+            {
+                Console.WriteLine("{0}: {1}", date.ToString(dateFormat), total);
+            };
+
+            int count = 0;
+            DateTime currentDate = DateTime.MinValue;
+            double currentTotal = 0;
+
+            foreach (string line in File.ReadLines(filePath).Skip(1))
+            {
+                DateTime date = getDate(line);
+                double amount = getAmount(line);
+
+                if (date != currentDate)
+                {
+                    if (count > 0)
+                    {
+                        displayTotal(currentDate, currentTotal);
+                        currentTotal = 0;
+                        count = 0;
+                    }
+                    currentDate = date;
+                }
+                currentTotal += amount;
+                count++;
+
+                if (count >= batchSize)
+                {
+                    string tempFilePath = string.Format("transactions_{0}.csv", currentDate.ToString("yyyyMMdd"));
+                    using (StreamWriter writer = new StreamWriter(tempFilePath, true))
+                    {
+                        writer.WriteLine("{0}, {1}", currentDate.ToString(dateFormat), currentTotal);   
+                    }
+                    currentTotal = 0;
+                    count = 0;
+                }
+            }
+            if (count > 0)
+            {
+                displayTotal(currentDate, currentTotal);
+
+                string tempFilePath = string.Format("transactions_{0}.csv", currentDate.ToString("yyyyMMdd"));
+                using (StreamWriter writer = new StreamWriter(tempFilePath, true))
+                {
+                    writer.WriteLine("{0},{1}", currentDate.ToString(dateFormat), currentTotal);
+                }
+            }
         }
     }
 }
--- Lab_3-1/Properties/AssemblyInfo.cs
+++ Lab_3-1/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4874c8f9-1414-493f-bf5c-84165f479bd6")]
+[assembly: Guid("6e0246bf-33cb-49a5-bac3-3380936b7e51")]
 
 // Version information for an assembly consists of the following four values:
 //
