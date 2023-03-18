47029cadf01f4acc28f7dead754d864c494672ca
naliwka <avierina.nataliia@gmail.com>
2023-03-18 19:28:29 +0200

    Add photo additing functions (gray & flip)

--- Lab_3-3/Program.cs
+++ Lab_3-3/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
 
 namespace Lab_3_3
 {
@@ -10,6 +8,58 @@ namespace Lab_3_3
     {
         static void Main(string[] args)
         {
+            string[] imageFiles = Directory.GetFiles("images");
+            int k = 0;
+
+            Func<Bitmap, Bitmap> grayScale = image =>
+            {
+                Bitmap grayBitmap = new Bitmap(image.Width, image.Height);
+
+                for (int i = 0; i < image.Width; i++)
+                {
+                    for (int j = 0; j < image.Height; j++)
+                    {
+                        Color pixel = image.GetPixel(i, j);
+                        int gray = (int)(pixel.R * 0.3 + pixel.G * 0.59 + pixel.B * 0.11);
+                        grayBitmap.SetPixel(i, j, Color.FromArgb(gray, gray, gray));
+                    }
+                }
+                return grayBitmap;
+            };
+
+            /*Func<Bitmap, Bitmap> flipHorizontal = image =>
+            {
+                Bitmap flippedBitmap = new Bitmap(image.Width, image.Height);
+
+                for (int i = 0; i < image.Width; i++)
+                {
+                    for (int j = 0; j < image.Height; j++)
+                    {
+                        flippedBitmap.SetPixel(image.Width - i - 1, j, image.GetPixel(i, j));
+                    }
+                }
+
+                return flippedBitmap;
+            };*/
+            
+            Action<Bitmap> displayImage = image =>
+            {
+                image.Save($"images\\output_{Path.GetFileNameWithoutExtension(imageFiles[k])}.png");           
+            };
+
+            for (int i = 0; i < imageFiles.Length; i++)
+            {
+                Bitmap original = new Bitmap(imageFiles[i]);
+                Bitmap gray = grayScale(original);
+                //Bitmap flipped = flipHorizontal(original);
+
+                displayImage(gray);
+                //displayImage(flipped);
+
+                k++;
+            }
+
+            Console.WriteLine("Image processing complete.");
         }
     }
 }
